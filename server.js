8d570d2da3a95bdbd5bd0e5168136369e1d58373
At <)>
2020-04-10 23:52:47 +0530

    hrk

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const fs=require('fs');
 
 const app=express();
-const port=process.env.port||80;
+const port=process.env.PORT||8080;
 
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
